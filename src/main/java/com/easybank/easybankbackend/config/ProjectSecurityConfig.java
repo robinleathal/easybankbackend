69c7f30ff5d834138c20c678a9ab40a1cbf4c270
Rabin Awal <morbid_leathal@hotmail.com>
2023-02-10 19:28:17 -0600

    Removed InMemoryUserDetailsManager in favor of JdbcUserDetailsManager

--- src/main/java/com/easybank/easybankbackend/config/ProjectSecurityConfig.java
+++ src/main/java/com/easybank/easybankbackend/config/ProjectSecurityConfig.java
@@ -5,10 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -28,21 +32,28 @@ public class ProjectSecurityConfig {
         return httpSecurity.build();
     }
 
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         //Approach 1 - Not recommended - use deprecated withDefaultPasswordEncoder() while creating user details
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin")
-//                .password("test1")
-//                .authorities("admin")
-//                .build();
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("test2")
-//                .authorities("user")
-//                .build();
-//        return new InMemoryUserDetailsManager(admin, user);
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("test1")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("test2")
+                .authorities("user")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
 
+
+    }*/
+    //
+    /*
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
         //Approach 2 - Not recommended - use deprecated noOpPasswordEncoder() while creating user details
         UserDetails admin = User.withUsername("admin")
                 .password("test1")
@@ -53,8 +64,13 @@ public class ProjectSecurityConfig {
                 .authorities("user")
                 .build();
         return new InMemoryUserDetailsManager(admin, user);
-    }
+    }*/
 
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+
+        return new JdbcUserDetailsManager(dataSource);
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
